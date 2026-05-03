bdcedfb8ebfcde7457d941172faea2ab2104121c
reyhansyah <reyhansyah23@gmail.com>
2026-05-03 17:43:32 +0700

    add function waiting connection

--- etl/postgre_loader.py
+++ etl/postgre_loader.py
@@ -1,13 +1,32 @@
 from sqlalchemy import create_engine
 import pandas as pd
-
+import time
 
 class PostgresLoader:
     def __init__(self, user, password, host, port, database):
         self.connection_string = (
             f"postgresql://{user}:{password}@{host}:{port}/{database}"
         )
+        self.wait_for_postgres()
         self.engine = create_engine(self.connection_string)
+
+    def wait_for_postgres(self, retries=10, delay=3):
+        print("Validating database connection...")
+
+        for attempt in range(1, retries + 1):
+            try:
+                engine = create_engine(self.connection_string)
+                with engine.connect() as conn:
+                    print("Connection successful!")
+                    engine.dispose()
+                    return
+            except Exception as e:
+                print(f"Attempt {attempt}/{retries} failed: {e}")
+                time.sleep(delay)
+
+        print("\n[CRITICAL ERROR] Could not connect to PostgreSQL after retries.")
+        import sys
+        sys.exit(1)
 
     def load_dataframe(self, df, table_name, schema = "public", if_exists = "append"):
         """
